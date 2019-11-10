6f66f03317e3c03c1cb0cc48e7372278c15df3cd
Fleford <fleford@gmail.com>
2019-11-10 16:38:43 -0700

    got a saved model to run

--- gan_for_gradient_based_inversion/training/run_loaded_nnmodel.py
+++ gan_for_gradient_based_inversion/training/run_loaded_nnmodel.py
@@ -1,18 +1,26 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
-from nnmodels import netD
 from nnmodels import netG
 
+"""
+Here's some code to practice loading and using trained GAN models
+"""
+
 # Inference device
-device = torch.device("cuda")
+device = torch.device("cpu")
 
 # NetG parameters
-nc = 1
-nz = 1
-ngf = 64
-gfs = 10
-ngpu = 1
+nc = 1  # 'number of channeles in original image space'
+nz = 1  # 'number of non-spatial dimensions in latent space z'
+ngf = 64    # 'initial number of filters for dis'
+gfs = 10    # 'kernel size for gen'
+ngpu = 1    # 'number of GPUs to use'
+
+batch_size = 1
+zx = 3
+zy = 3
 
 # File directory
 outf = './train_data'
@@ -20,7 +28,7 @@ epoch = 0
 
 # Load model
 netG = netG(nc, nz, ngf, gfs, ngpu)
-netG.load_state_dict(torch.load('%s/netG_epoch_%d.pth' % (outf, epoch), map_location="cuda:0"))
+netG.load_state_dict(torch.load('%s/netG_epoch_%d.pth' % (outf, epoch)))
 netG.to(device)
 netG.eval()
 # netG.to(device)
@@ -28,26 +36,28 @@ print(netG)
 print()
 
 # Load input matrix
-input_matrix = torch.ones((1, 1, 353, 353))
-input_matrix = input_matrix.to(device)
+noise = torch.rand(batch_size, nz, zx, zy, device=device)*2-1
 # input_matrix.to(device)
-print("Input matrix:")
-print(input_matrix)
-print(input_matrix.shape)
+print("noise matrix:")
+print(noise)
+print(noise.shape)
 print()
 
+# Turn off gradient calculation
+torch.set_grad_enabled(False)
+
 # forward run the model
-output = netG(input_matrix)
+output = netG(noise)
 print("Output matrix:")
 print(output)
+print(output.shape)
+print()
 
-# device = torch.device("cuda")
-# model = TheModelClass(*args, **kwargs)
-# model.load_state_dict(torch.load(PATH, map_location="cuda:0"))  # Choose whatever GPU device number you want
-# model.to(device)
-# # Make sure to call input = input.to(device) on any input tensors that you feed to the model
-
-
+numpy_output = output.numpy()
+numpy_output = numpy_output.squeeze()
+print("numpy_output:")
+print(numpy_output)
+print(numpy_output.shape)
 
 
 
