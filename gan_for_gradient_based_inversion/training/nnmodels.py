The mismatch of the dimensions for the condition matrix was fixed with broadcasting

gan_for_gradient_based_inversion/training/nnmodels.py
@@ -88,7 +88,13 @@ class netG(nn.Module):
 
         self.main = nn.Sequential(
 
-                nn.ConvTranspose2d(     nz, ngf * 8, gfs, 2, 2, bias=False),
+                nn.ConvTranspose2d(     nz, ngf * 8, gfs, 2, 1, bias=False),
+                # nn.ConvTranspose2d(nz, ngf * 8, gfs, 2,  gfs//2, bias=False),
+                nn.ReLU(True),
+                nn.InstanceNorm2d(ngf * 8),
+
+                # Remove later
+                nn.ConvTranspose2d(ngf * 8, ngf * 8, gfs, 2, 1, bias=False),
                 # nn.ConvTranspose2d(nz, ngf * 8, gfs, 2,  gfs//2, bias=False),
                 nn.ReLU(True),
                 nn.InstanceNorm2d(ngf * 8),
@@ -217,6 +223,7 @@ class netG_transformer(nn.Module):
         )
 
     def forward(self, input, condition):
+        condition = torch.ones_like(input) * condition
         input_to_G = torch.cat((input, condition), 1)
         if input_to_G.is_cuda and self.ngpu > 1:
             output_from_G = nn.parallel.data_parallel(self.main, input_to_G,

gan_for_gradient_based_inversion/training/train2d_with_conditioning.py
@@ -23,6 +23,7 @@ from nnmodels import netG_transformer
 import numpy as np
 from utils import get_texture2D_iter, zx_to_npx
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--batchSize', type=int, default=16, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
@@ -139,7 +140,7 @@ def generate_condition(input_matrix):
     output_matrix = ref_k_array * random_matrix
 
     # output_matrix = torch.zeros_like(input_matrix)
-    return output_matrix
+    return output_matrix.cuda()
 
 
 # input_noise = torch.rand(batch_size, nz, zx, zy, device=device)*2-1