bfb224d09ab22099b954ed2d7444f8d9cf5cca29
Fleford <fleford@gmail.com>
2020-05-21 13:53:47 -0600

    mean and variance of the conditioned outputs

--- gan_for_gradient_based_inversion/training/run_loaded_nnmodel_conditioned.py
+++ gan_for_gradient_based_inversion/training/run_loaded_nnmodel_conditioned.py
@@ -45,8 +45,9 @@ def generate_condition(ref_k_array):
     # ref_k_array = np.loadtxt("k_array_ref_gan.txt")
     ref_k_array = torch.as_tensor(ref_k_array, dtype=torch.float32)
     random_matrix = torch.randint_like(ref_k_array, 2)
-    for x in range(4):
+    for x in range(5):
         random_matrix = random_matrix * torch.randint_like(ref_k_array, 2)
+    print("Number of nonzero elements: ", np.count_nonzero(random_matrix))
     output_matrix = ref_k_array * random_matrix
     plt.matshow(output_matrix)
 
@@ -86,7 +87,7 @@ print("numpy_output:")
 print(numpy_output)
 print(numpy_output.shape)
 print()
-# plt.matshow(numpy_output)
+plt.matshow(numpy_output)
 # plt.show()
 
 # Prepare stack
