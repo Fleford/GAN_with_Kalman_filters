208108d55785e65112c7ce3a50dc5776f3cfb142
Fleford <fleford@gmail.com>
2021-07-02 12:10:34 -0500

    added log

--- 3D_Latent_Space_GAN/run_model.py
+++ 3D_Latent_Space_GAN/run_model.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 from torch import nn, optim
-from progan_modules_copy import Generator
+from progan_modules import Generator
 from utils import get_texture2D_iter
 from matplotlib import pyplot as plt
 
@@ -28,56 +28,82 @@ def generate_condition(input_matrix, density=10):
 device = 'cuda:0'
 # device = 'cpu'
 b_size = 32
-input_code_size = 128
+input_z_size = 32
 
-generator = Generator(in_channel=64, input_code_dim=128, pixel_norm=False, tanh=False).to(device)
+generator = Generator(in_channel=128, input_z_channels=input_z_size, pixel_norm=False, tanh=False).to(device)
+
+generator.load_state_dict(torch.load('trial_test18_2021-06-24_12_58/checkpoint/400000_g.model'))
+
+# Prepare z vectors for training
+gen_z_first_half = torch.randn(b_size//2, input_z_size, 2, 2).to(device)
+gen_z_top_swap = torch.flip(gen_z_first_half[:, :, 0:gen_z_first_half.shape[2]//2, :], dims=[0])
+gen_z_bttm = gen_z_first_half[:, :, gen_z_first_half.shape[2]//2:gen_z_first_half.shape[2], :]
+gen_z_second_half = torch.cat((gen_z_top_swap, gen_z_bttm), dim=2)
+gen_z = torch.cat((gen_z_first_half, gen_z_second_half), dim=0)
+
+print(gen_z_top_swap.shape)
+print(gen_z_bttm.shape)
+print(gen_z_first_half[0, 0])
+print(gen_z_first_half[-1, 0])
+print(gen_z_top_swap[0, 0])
+print(gen_z_bttm[0, 0])
+print(gen_z_second_half[0, 0])
+print(gen_z_second_half.shape)
+print(gen_z.shape)
+print(gen_z[gen_z.shape[0]//2, 0])
 
 
-# generator.load_state_dict(torch.load('trial_test18_2020-10-12_22_29/checkpoint/160000_g.model'))
-generator.load_state_dict(torch.load('trial_test18_2020-10-18_17_37/checkpoint/140000_g.model'))
+#3 Prep imgs for spatial loss function
+gen_imgs_first_half = torch.randn(b_size//2, input_z_size, 4, 4).to(device)
+gen_imgs_top_swap = torch.flip(gen_imgs_first_half[:, :, 0:gen_imgs_first_half.shape[2]//2, :], dims=[0])
+gen_imgs_bttm = gen_imgs_first_half[:, :, gen_imgs_first_half.shape[2]//2:gen_imgs_first_half.shape[2], :]
+gen_imgs_second_half = torch.cat((gen_imgs_top_swap, gen_imgs_bttm), dim=2)
+gen_imgs = torch.cat((gen_imgs_first_half, gen_imgs_second_half), dim=0)
+# gen_img_true_z_swap = gen_imgs_top_swap_bttm[gen_imgs_top_swap_bttm.shape[0]//2:, :, :, :]
+
+# print(gen_img_true_z_swap.shape)
+# print(gen_img_true_z_swap[0, 0])
+print(gen_imgs_top_swap.shape)
+print(gen_imgs_bttm.shape)
+print(gen_imgs_first_half[0, 0])
+print(gen_imgs_first_half[-1, 0])
+print(gen_imgs_top_swap[0, 0])
+print(gen_imgs_bttm[0, 0])
+print(gen_imgs_second_half[0, 0])
+print(gen_imgs[gen_imgs.shape[0]//2, 0])
+print(gen_imgs.shape)
+
+breakpoint()
 
 # sample input data: vector for Generator
-gen_z = torch.randn(b_size, input_code_size).to(device)
+gen_z_a = torch.randn(b_size, input_z_size, 2, 2).to(device)
+gen_z_b = torch.randn(b_size, input_z_size, 2, 2).to(device)
 
-# generate condition array
-data_iter = get_texture2D_iter('ti/', batch_size=b_size)
-real_image_raw_res = torch.Tensor(next(data_iter)).to(device)
-cond_array, cond_mask = generate_condition(real_image_raw_res)
+gen_z_a_rot180 = torch.rot90(gen_z_a, k=2, dims=(2, 3))
 
-cond_downsampler = torch.nn.MaxPool2d((8, 8), stride=(8, 8))
+# gen_z_a_top = gen_z_a[:,:,0,:]
+# gen_z_b_bttm = gen_z_b[:,:,1,:]
+# gen_z_ab = torch.stack((gen_z_a_top, gen_z_b_bttm), dim=2)
 
-# broadcast first cond_array to whole batch
-one_cond_array = torch.zeros_like(cond_array)
-for slice in range(len(cond_array)):
-    one_cond_array[slice] = cond_array[0]
-cond_array = one_cond_array
+gen_z_ab = 0.9 * gen_z_a + 0.1 * gen_z_b
 
-# broadcast first cond array to the whole batch (cond_mask)
-one_cond_mask = torch.zeros_like(cond_mask)
-for slice in range(len(cond_mask)):
-    one_cond_mask[slice] = cond_mask[0]
-cond_mask = one_cond_mask
-
-ds_mask = cond_downsampler(cond_mask)
+print(gen_z_a[0, 0])
+print(gen_z_a_rot180[0, 0])
+# print(gen_z_b[0, 0])
+# print(gen_z_ab[0, 0])
 
 # generate conditioned images
-fake_image = generator(gen_z, one_cond_array, step=5, alpha=1.0)
+fake_image_a = generator(gen_z_a, step=7, alpha=1.0)
+fake_image_a_rot180 = generator(gen_z_a_rot180, step=7, alpha=1.0)
+# fake_image_b = generator(gen_z_b, step=7, alpha=1.0)
+# fake_image_ab = generator(gen_z_ab, step=7, alpha=1.0)
 
 # # plot sample of fake images
-plt.matshow(real_image_raw_res[0, 0].cpu().detach().numpy())
-plt.matshow(cond_array[0, 0].cpu().detach().numpy())
-# plt.matshow(ds_mask[0, 0].cpu().detach().numpy())
-plt.matshow(fake_image[0, 0].cpu().detach().numpy())
-# plt.matshow(cond_mask[0, 0].cpu().detach().numpy())
-# plt.show()
+plt.matshow(fake_image_a[0, 0].cpu().detach().numpy())
+plt.matshow(fake_image_a_rot180[0, 0].cpu().detach().numpy())
+# plt.matshow(fake_image_b[0, 0].cpu().detach().numpy())
+# plt.matshow(fake_image_ab[0, 0].cpu().detach().numpy())
 
-# Calculate statistics
-images = fake_image.cpu().squeeze().detach().numpy()
-avg_img = np.mean(images, axis=0)
-std_img = np.std(images, axis=0)
-
-plt.matshow(avg_img)
-plt.matshow(std_img)
 plt.show()
 
 breakpoint()
